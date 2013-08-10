26bfd7b0cc3d96d0f5fc993350d056a5e26a2afe
alexrp <alex@lycus.org>
2013-08-10 22:20:25 +0200

    Make EXMAKE_PATH a colon-separated list.
    Also add some more docs to ExMake.Libraries.

--- lib/libraries.ex
+++ lib/libraries.ex
@@ -2,6 +2,21 @@ defmodule ExMake.Libraries do
     @moduledoc """
     Contains functions to manage the loading of 'libraries'; that is, modules
     that script files can `use` and call macros/functions in.
+
+    By convention, such modules begin with `ExMake.Lib.` and are followed by
+    some named describing what the module contains, e.g. `ExMake.Lib.C`.
+
+    By default, ExMake looks for libraries in:
+
+    * `/usr/local/lib/exmake`
+    * `/usr/lib/exmake`
+    * `/lib/exmake`
+
+    Additionally, if the `HOME` environment variable is defined, it will look
+    in `$HOME/.exmake`.
+
+    The `EXMAKE_PATH` environment variable can be set to a colon-separated
+    list of paths to use. When it is set, the above paths will not be considered.
     """
 
     @doc """
@@ -12,7 +27,7 @@ defmodule ExMake.Libraries do
         if s = System.get_env("EXMAKE_PATH") do
             ExMake.Logger.debug("Using EXMAKE_PATH: #{s}")
 
-            [s]
+            Enum.filter(String.split(s, ":"), fn(s) -> s != "" end)
         else
             p = [Path.join(["/usr", "local", "lib", "exmake"]),
                  Path.join(["/usr", "lib", "exmake"]),
