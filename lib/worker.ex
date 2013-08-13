c19f8bd7935e7bdb3bc051da6c335692e9257aca
alexrp <alex@lycus.org>
2013-08-13 10:38:37 +0200

    Fix broken logic in 80b0e50355261d1ba199eed23614103db6922413.

--- lib/worker.ex
+++ lib/worker.ex
@@ -94,7 +94,7 @@ defmodule ExMake.Worker do
                         {_, r2} = :digraph.vertex(g, v2)
 
                         cond do
-                            t = r2[:targets] -> t in src
+                            t = r2[:targets] -> src in t
                             n = r2[:name] -> n == src
                             true -> false
                         end
