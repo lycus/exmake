3523b72b92ed74cab0e584b3f118fef0456e3516
alexrp <alex@lycus.org>
2013-08-19 08:43:23 +0200

    Make the ExMake library as flexible as the Elixir library.
    * Support specifying output directory.
    * Support 'odd', non-Elixir module names.
    * Support explicit dependencies.

--- lib/lib/exmake.ex
+++ lib/lib/exmake.ex
@@ -7,16 +7,24 @@ defmodule ExMake.Lib.ExMake do
     url "https://github.com/lycus/exmake"
     author "Alex Rønne Petersen", "alex@lycus.org"
 
-    defmacro exm_lib(src, mods) do
+    defmacro exm_lib(src, mods, opts // []) do
         quote do
+            @exm_exmake_opts unquote(opts)
+
             src = unquote(src)
-            mods = Enum.map(unquote(mods), fn(m) -> "Elixir." <> m <> ".beam" end)
+            srcs = [src] ++ (@exm_exmake_opts[:deps] || [])
+            mods = unquote(mods) |>
+                   Enum.map(fn(m) -> m <> ".beam" end) |>
+                   Enum.map(fn(m) -> (@exm_exmake_opts[:output_dir] || Path.dirname(src)) |>
+                                     Path.join(m) end)
 
             rule mods,
-                 [src],
-                 [src], _, dir do
+                 srcs,
+                 [src | _], _, dir do
+                output_dir = if s = @exm_exmake_opts[:output_dir], do: Path.join(dir, s), else: Path.dirname(src)
+
                 Enum.each(Code.compile_string(File.read!(src), src), fn({mod, code}) ->
-                    File.write!(Path.join(dir, atom_to_binary(mod) <> ".beam"), code)
+                    File.write!(Path.join(output_dir, atom_to_binary(mod) <> ".beam"), code)
                 end)
             end
         end
