f91402f9739e616c7a22c10e55843abb9965e29b
alexrp <alex@lycus.org>
2013-09-07 04:51:04 +0200

    Fix host architecture detection.

--- lib/lib/host.ex
+++ lib/lib/host.ex
@@ -41,7 +41,7 @@ defmodule ExMake.Lib.Host do
 
         sys_arch = :erlang.system_info(:system_architecture) |>
                    String.from_char_list!() |>
-                   String.split() |>
+                   String.split("-") |>
                    Enum.first()
 
         re = fn(re) -> Regex.match?(re, sys_arch) end
